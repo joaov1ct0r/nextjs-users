2b6b6fc758177ba9804bab656487bd373b748e32
joaov1ct0r <jov9051@outlook.com>
2024-10-11 15:15:27 -0300

    feat: not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex justify-center items-center h-svh">
+      <h1 className="text-5xl text-white">Page not found</h1>
+    </div>
+  );
+}
