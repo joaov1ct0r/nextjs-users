207c3c6881c779a798b34202b12d255ddd11c869
joaov1ct0r <jov9051@outlook.com>
2024-10-14 19:03:14 -0300

    feat: sign-in-user api

--- src/app/signin/api/sign-in-user.ts
+++ src/app/signin/api/sign-in-user.ts
@@ -2,7 +2,6 @@ import { Dispatch } from "react";
 import { Action } from "@/app/signin/interfaces/action";
 import SignInUser from "@/app/signin/interfaces/sign-in-user";
 import { api } from "@/app/lib/axios";
-import { toast } from "react-toastify";
 
 export async function signInUser(dispatch: Dispatch<Action>, user: SignInUser) {
   dispatch({ type: "fetch_start" });
@@ -11,13 +10,10 @@ export async function signInUser(dispatch: Dispatch<Action>, user: SignInUser) {
 
     if (response.status !== 200) {
       dispatch({ type: "fetch_error", error: "Failed to sign in user" });
-      toast.error("Failed to sign in user");
     }
 
     dispatch({ type: "fetch_success", user: response.data.resource });
-    toast.success("Signed in with success");
   } catch (error) {
     dispatch({ type: "fetch_error", error: "Failed to sign in user" });
-    toast.error("Failed to sign in user");
   }
 }
