753188345e99c3e3655e04064c8da0d92c9a6e6f
joaov1ct0r <jov9051@outlook.com>
2024-10-14 19:03:37 -0300

    feat: sign-in-form

--- src/app/signin/components/sign-in-form.tsx
+++ src/app/signin/components/sign-in-form.tsx
@@ -1,10 +1,73 @@
 "use client";
 
-import React from "react";
+import React, {
+  ChangeEvent,
+  useState,
+  MouseEvent,
+  useEffect,
+  useCallback,
+} from "react";
 import InputForm from "@/app/components/input-form";
 import ButtonForm from "@/app/components/button-form";
+import SignInUser from "@/app/signin/interfaces/sign-in-user";
+import { useSignInCtx } from "@/app/signin/hooks/use-sign-in";
+import { useSignInDispatch } from "@/app/signin/hooks/use-sign-in-dispatch";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
+  const router = useRouter();
+  const [credentials, setCredentials] = useState<SignInUser>({
+    username: "",
+    password: "",
+  });
+
+  const { error, success } = useSignInCtx();
+  const { signInUser } = useSignInDispatch();
+
+  const handleFormChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setCredentials({
+      ...credentials,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    signInUser(credentials);
+    clearCredentials();
+  };
+
+  const clearCredentials = () => {
+    setCredentials({
+      username: "",
+      password: "",
+    });
+  };
+
+  const handleSignUp = () => {
+    router.push("/signup");
+  };
+
+  const memoizedHandleSignIn = useCallback(() => {
+    router.push("/about");
+  }, [router]);
+
+  useEffect(() => {
+    if (error !== null) {
+      toast.error(error);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (success !== null && success === true && error === null) {
+      toast.success("User signed in with success!");
+      memoizedHandleSignIn();
+    }
+  }, [memoizedHandleSignIn, error, success]);
+
   return (
     <div className="w-full max-w-xs">
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -12,29 +75,31 @@ export default function LoginForm() {
           placeholder="User"
           id="username"
           type="text"
-          handleOnChange={() => null}
+          handleOnChange={handleFormChange}
           name="username"
+          value={credentials.username}
         />
         <InputForm
           placeholder="Password"
           id="password"
           type="password"
-          handleOnChange={() => null}
+          handleOnChange={handleFormChange}
           name="password"
+          value={credentials.password}
         />
 
         <div className="flex items-center justify-between">
           <ButtonForm
             model="success"
             placeholder="Sign in"
-            type="submit"
-            handleOnClick={() => null}
+            type="button"
+            handleOnClick={handleFormSubmit}
           />
           <ButtonForm
             type="button"
             model="warning"
             placeholder="Sign up"
-            handleOnClick={() => null}
+            handleOnClick={handleSignUp}
           />
         </div>
       </form>
