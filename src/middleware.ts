da541b764d64d846860c3453c84bdad1daedc4d4
joaov1ct0r <jov9051@outlook.com>
2024-11-04 12:43:29 -0300

    feat: added req checks to middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,11 @@ import { NextResponse } from "next/server";
 
 export default function middleware(request: NextRequest) {
   const authorization = request.cookies.get("authorization");
+  console.log("authorization", authorization);
   const user = request.cookies.get("user");
+  console.log("user", user);
   const userObj = request.cookies.get("userObj");
+  console.log("userObj", userObj);
 
   if (
     authorization &&
@@ -12,19 +15,23 @@ export default function middleware(request: NextRequest) {
     userObj &&
     request.nextUrl.pathname !== "/about"
   ) {
+    console.log("send to /about");
     return NextResponse.redirect(new URL("/about", request.url));
   }
 
   const cookiesNotFound = !authorization || !user;
+  console.log("cookiesNotFound", cookiesNotFound);
 
   if (
     cookiesNotFound &&
     request.nextUrl.pathname !== "/" &&
     request.nextUrl.pathname !== "/signup"
   ) {
+    console.log("send to /");
     return NextResponse.redirect(new URL("/", request.url));
   }
 
+  console.log("send to next req");
   return NextResponse.next();
 }
 
