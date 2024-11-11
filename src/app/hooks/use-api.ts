d660eb78c464a71b68f58a3fb504818b42ad8409
joaov1ct0r <jov9051@outlook.com>
2024-11-11 00:59:18 -0300

    refactor: use-api removed signal controller

--- src/app/hooks/use-api.ts
+++ src/app/hooks/use-api.ts
@@ -1,59 +1,42 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
-import { api, controller } from "@/app/lib/axios";
+import { api } from "@/app/lib/axios";
 import { clearCookies } from "@/app/utils/cookies";
 
 export const useApi = () => {
   const router = useRouter();
 
   useEffect(() => {
-    api.interceptors.request.use(
-      (config) => {
-        console.log("config", config);
-        if (config.method?.toUpperCase() === "OPTIONS") {
-          controller.abort();
-        }
-
-        return config;
-      },
-      (error) => {
-        console.log("config error", error);
-        return Promise.reject(error);
-      },
-    );
-
     const interceptor = api.interceptors.response.use(
-      (response) => {
-        console.log("response", response);
+      async (response) => {
         if (
           response.data.message &&
           response.config.method?.toUpperCase() !== "OPTIONS"
         ) {
           toast.success(response.data.message);
-          return response;
+          return Promise.resolve(response);
         }
 
         toast.success("Success");
-        return response;
+        return Promise.resolve(response);
       },
       async (error) => {
-        console.log("response error", error);
         if (error?.response?.status === 401) {
           toast.error("Unauthorized");
-          return;
+          return Promise.reject(error);
         }
 
         if (error?.response?.status === 403) {
           toast.error("Forbidden");
           await clearCookies();
           router.push("/");
-          return;
+          return Promise.reject(error);
         }
 
         if (error?.response?.data?.message) {
           toast.error(error.response.data.message);
-          return;
+          return Promise.reject(error);
         }
 
         toast.error("Failed");
--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const controller = new AbortController();
-
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || "https://api.crud.shop";
 const api = axios.create({
   baseURL: apiUrl,
   withCredentials: true,
-  signal: controller.signal,
 });
 
-export { api, controller };
+export { api };
