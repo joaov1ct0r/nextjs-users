7e8e4b84859fc252bc0d4a07c96ec2afe808c024
joaov1ct0r <jov9051@outlook.com>
2024-11-11 01:16:02 -0300

    feat: moved ToastContainer out of layout to main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import Header from "@/app/components/header";
 import Container from "@/app/components/container";
 import Providers from "@/app/providers";
-import { ToastContainer, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Suspense } from "react";
 import Loading from "@/app/components/loading";
@@ -21,22 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`sans-serif antialiased`}>
-        <header>
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            limit={0}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss={false}
-            draggable
-            pauseOnHover
-            theme="dark"
-            transition={Bounce}
-          />
-        </header>
         <Providers>
           <Container>
             <Header />
