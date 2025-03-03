a511357d6654e8fb0a86d8390a3494e5cf225dd7
joaov1ct0r <jov9051@outlook.com>
2025-03-03 00:58:11 -0300

    feat: updated fix index in sign in components

--- src/app/signin/components/index.ts
+++ src/app/signin/components/index.ts
@@ -1,15 +1,21 @@
-import InputForm from "@/app/components/input-form";
-import ButtonForm from "@/app/components/button-form";
+import { InputForm } from "@/app/components/input-form";
+import { ButtonForm } from "@/app/components/button-form";
+import { LabelForm } from "@/app/components/label-form"
+import { ButtonWrapper } from "@/app/components/button-wrapper/button-wrapper"
+import { InputWrapper } from "@/app/components/input-wrapper/input-wrapper"
 import { SignInRoot } from "@/app/signin/components/root/root";
 import { SignInForm } from "@/app/signin/components/form/form"
 import { SignInContent } from "@/app/signin/components/content/content"
-import { ActionsButton } from "@/app/signin/components/actions-button/actions-button"
+import { ForgetPasswordModal } from "@/app/signin/components/forget-password-modal";
 
 export const SignIn = {
   Root: SignInRoot,
   Form: SignInForm,
   Content: SignInContent,
+  Label: LabelForm,
+  InputWrapper,
   Input: InputForm,
-  ActionsButton: ActionsButton,
-  ActionButton: ButtonForm
+  ButtonWrapper,
+  Button: ButtonForm,
+  ForgetPasswordModal
 };
