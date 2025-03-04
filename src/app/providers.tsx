9503a8ef0bd37ed075744fbaa6158a5b28046e54
joaov1ct0r <jov9051@outlook.com>
2025-03-04 19:16:46 -0300

    feat: added forget-password provider to main providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { SignUpProvider } from "@/app/signup/contexts/sign-up-context";
 import { SignInProvider } from "@/app/signin/contexts/sign-in-context";
 import { AboutProvider } from "@/app/about/contexts/about-context";
+import { ForgetPasswordProvider } from "@/app/forget-password/contexts/forget-password-context";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -10,9 +11,11 @@ interface ProvidersProps {
 export default async function Providers({ children }: ProvidersProps) {
   return (
     <SignInProvider>
-      <SignUpProvider>
-        <AboutProvider>{children}</AboutProvider>
-      </SignUpProvider>
+      <ForgetPasswordProvider>
+        <SignUpProvider>
+          <AboutProvider>{children}</AboutProvider>
+        </SignUpProvider>
+      </ForgetPasswordProvider>
     </SignInProvider>
   );
 }
