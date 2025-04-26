00ce6ad0d9b11157d0f1bf74307da16f49c797bf
joaov1ct0r <jov9051@outlook.com>
2025-04-26 01:10:46 -0300

    feat: added validation to only show menu when user is authenticated

--- src/app/components/menu/menu-wrapper/menu-wrapper.tsx
+++ src/app/components/menu/menu-wrapper/menu-wrapper.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { ReactNode } from "react";
-
+import { useSignInCtx } from "@/app/signin/hooks/use-sign-in";
 export interface MenuWrapperProps {
   children: ReactNode;
 }
 
 export function MenuWrapper({ children }: MenuWrapperProps) {
+  const { authenticated } = useSignInCtx();
   return (
-    <div className="mb-2 w-[15%] h-full bg-gray-50 flex flex-col items-center">
+    <div
+      className={
+        authenticated
+          ? "mb-2 w-[15%] h-full bg-gray-50 flex flex-col items-center"
+          : "hidden"
+      }
+    >
       {children}
     </div>
   );
