4932e3a2060a1f5489f26fef1b7da916677fc532
joaov1ct0r <jov9051@outlook.com>
2025-04-26 02:20:52 -0300

    feat: added routing to menu

--- src/app/components/menu/menu.tsx
+++ src/app/components/menu/menu.tsx
@@ -1,13 +1,30 @@
+"use client";
+
 import { MenuWrapper } from "@/app/components/menu/menu-wrapper/menu-wrapper";
 import { MenuButton } from "@/app/components/menu/menu-button/menu-button";
 import { FaAddressCard } from "react-icons/fa";
 import { FaNewspaper } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 export function Menu() {
+  const router = useRouter();
+
+  const handleFeedClick = () => {
+    router.push("/feed");
+  };
+
+  const handleAboutClick = () => {
+    router.push("/about");
+  };
+
   return (
     <MenuWrapper>
-      <MenuButton icon={<FaNewspaper />}>Feed</MenuButton>
-      <MenuButton icon={<FaAddressCard />}>About Me </MenuButton>
+      <MenuButton icon={<FaNewspaper />} onClick={handleFeedClick}>
+        Feed
+      </MenuButton>
+      <MenuButton icon={<FaAddressCard />} onClick={handleAboutClick}>
+        About Me
+      </MenuButton>
     </MenuWrapper>
   );
 }
