5f951b1db9060a69e684dfd5cf943969a7eca2ca
joaov1ct0r <jov9051@outlook.com>
2025-04-26 04:59:34 -0300

    feat: added feed provider to providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@ import { SignUpProvider } from "@/app/signup/contexts/sign-up-context";
 import { SignInProvider } from "@/app/signin/contexts/sign-in-context";
 import { AboutProvider } from "@/app/about/contexts/about-context";
 import { ForgetPasswordProvider } from "@/app/forget-password/contexts/forget-password-context";
+import { FeedProvider } from "@/app/feed/contexts/feed-context";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -13,7 +14,9 @@ export default async function Providers({ children }: ProvidersProps) {
     <SignInProvider>
       <ForgetPasswordProvider>
         <SignUpProvider>
-          <AboutProvider>{children}</AboutProvider>
+          <FeedProvider>
+            <AboutProvider>{children}</AboutProvider>
+          </FeedProvider>
         </SignUpProvider>
       </ForgetPasswordProvider>
     </SignInProvider>
