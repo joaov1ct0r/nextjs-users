c55fbac3c8b2dd2d920e069c6cdaac9e903a5540
joaov1ct0r <jov9051@outlook.com>
2025-04-26 03:06:18 -0300

    feat: created feed index

---
+++ src/app/feed/components/index.ts
@@ -0,0 +1,17 @@
+import { FeedRoot } from "@/app/feed/components/root/root"
+import { FeedContent } from "@/app/feed/components/content/content"
+import { InputWrapper } from "@/app/components/input-wrapper/input-wrapper"
+import { LabelForm } from "@/app/components/label-form"
+import { InputForm } from "@/app/components/input-form"
+import { ButtonWrapper } from "@/app/components/button-wrapper/button-wrapper"
+import { ButtonForm } from "@/app/components/button-form"
+
+export const Feed = {
+    Root: FeedRoot,
+    Content: FeedContent,
+    InputWrapper,
+    Label: LabelForm,
+    Input: InputForm,
+    ButtonWrapper,
+    Button: ButtonForm
+}
