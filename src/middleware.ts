a1c7cba6587922a1149d19b2fa1a65cb1fcbb739
joaov1ct0r <jov9051@outlook.com>
2025-04-26 02:20:37 -0300

    feat: added is-authenticated to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,15 @@
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { isAuthenticated } from "@/app/utils/is-authenticated";
 
 export default function middleware(request: NextRequest) {
-  const authorization = request.cookies.get("authorization");
-  const user = request.cookies.get("user");
-  const userObj = request.cookies.get("userObj");
-
-  const cookiesNotFound = !authorization || !user || !userObj;
-
-  if (
-    cookiesNotFound &&
-    request.nextUrl.pathname !== "/signin" &&
-    request.nextUrl.pathname !== "/signup" &&
-    request.nextUrl.pathname !== "/forget-password"
-  ) {
+  if (!isAuthenticated(request)) {
     return NextResponse.redirect(new URL("/signin", request.url));
-  }
-
-  if (
-    authorization &&
-    user &&
-    userObj &&
-    request.nextUrl.pathname !== "/about"
-  ) {
-    return NextResponse.redirect(new URL("/about", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images|signin|signup|forget-password).*)"],
 };
