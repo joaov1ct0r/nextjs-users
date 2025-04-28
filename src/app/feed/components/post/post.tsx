958d7a92c11b54bbaaba6ae50d3529036b0dfb3e
joaov1ct0r <jov9051@outlook.com>
2025-04-27 22:06:47 -0300

    feat: created post component

---
+++ src/app/feed/components/post/post.tsx
@@ -0,0 +1,58 @@
+import { Post as PostImp } from "@/app/interfaces/post";
+import { FaCalendar, FaUser } from "react-icons/fa";
+import { RxUpdate } from "react-icons/rx";
+
+export interface PostProps {
+  post: PostImp;
+}
+
+export function Post({ post }: PostProps) {
+  return (
+    <div
+      className="w-[50%] h-[20%] px-4 pt-3 pb-4 m-2 bg-white shadow-md rounded overflow-auto"
+      id={post.id}
+    >
+      <div
+        className="text-gray-700 text-sm flex flex-row items-center"
+        id="userInfo"
+      >
+        <FaUser />
+        <p id="userId">{post.userWhoCreatedId}</p>
+      </div>
+
+      <div
+        className="flex flex-row justify-between text-gray-700 text-sm"
+        id="dates"
+      >
+        <div
+          className="flex flex-row justify-evenly items-center"
+          id="createdAt_date"
+        >
+          <FaCalendar />
+          <p id="createdAt">
+            {post.createdAt.toLocaleString("pt-BR", {
+              timeZone: "America/Sao_Paulo",
+            })}
+          </p>
+        </div>
+
+        <div
+          className="flex flex-row justify-evenly items-center"
+          id="updatedAt_date"
+        >
+          <RxUpdate />
+          <p id="updatedAt">
+            {post.updatedAt
+              ? post.updatedAt.toLocaleString("pt-BR", {
+                  timeZone: "America/Sao_Paulo",
+                })
+              : ""}
+          </p>
+        </div>
+      </div>
+      <div className="text-black text-xl mt-2" id="content">
+        {post.content}
+      </div>
+    </div>
+  );
+}
