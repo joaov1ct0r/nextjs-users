84c25172ad29af767cb54076e23824aa343d19a1
joaov1ct0r <jov9051@outlook.com>
2025-05-11 19:35:26 -0300

    feat: added infinite scrolling to posts

--- src/app/feed/components/posts/posts.tsx
+++ src/app/feed/components/posts/posts.tsx
@@ -1,11 +1,22 @@
 import { Post } from "@/app/feed/components/post/post";
 import { useFeedCtx } from "@/app/feed/hooks/use-feed";
 import { useFeedDispatch } from "@/app/feed/hooks/use-feed-dispatch";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 export function Posts() {
   const { getPosts } = useFeedDispatch();
   const { posts, error, success, loading } = useFeedCtx();
+
+  const handleScroll = useCallback(() => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >=
+      document.documentElement.scrollHeight
+    ) {
+      (async () => {
+        await getPosts();
+      })();
+    }
+  }, [getPosts]);
 
   useEffect(() => {
     if (error === null && success === null && loading === false) {
@@ -15,11 +26,18 @@ export function Posts() {
     }
   });
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
   return (
     <div className="size-full flex flex-col items-center">
       {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
+      {loading && <p>Loading...</p>}
     </div>
   );
 }
